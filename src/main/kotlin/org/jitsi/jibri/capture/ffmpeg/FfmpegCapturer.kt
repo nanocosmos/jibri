864a58675527a59ec54a20012bc875e0d888e39c
ol <lietz@nanocosmos.de>
2020-10-20 00:38:43 +0200

    1080p / Full HD screen capture and encoding

--- src/main/kotlin/org/jitsi/jibri/capture/ffmpeg/FfmpegCapturer.kt
+++ src/main/kotlin/org/jitsi/jibri/capture/ffmpeg/FfmpegCapturer.kt
@@ -38,8 +38,10 @@ import java.util.logging.Logger
  * Parameters which will be passed to ffmpeg
  */
 data class FfmpegExecutorParams(
-    val resolution: String = "1280x720",
-    val framerate: Int = 30,
+    //val resolution: String = "1280x720",
+    val resolution: String = "1920x1080",
+    //val framerate: Int = 30,
+    val framerate: Int = 25,
     val videoEncodePreset: String = "veryfast",
     val queueSize: Int = 4096,
     val streamingMaxBitrate: Int = 2976,
